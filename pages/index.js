149b2dff65e7f8c8bc2b4ec45dcb49a5d2a5d9be
Benjamin Desole <benjamin.desole@gmail.com>
2023-06-26 21:39:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from '../lib/posts';
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const response = await fetch(
+    "https://newsapi.org/v2/everything?q=motogp&from=2023-05-26&sortBy=publishedAt&apiKey=2474fba8daed4dbfa7136f82eb4d6491",
+    requestOptions
+  );
+  const news = await response.json();
   return {
     props: {
       allPostsData,
+      news,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, news }) {
   return (
     <Layout home>
       {/* Keep the existing code here */}
@@ -29,6 +40,14 @@ export default function Home({ allPostsData }) {
               <br />
               {date}
             </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2 className={utilStyles.headingLg}>News</h2>
+        <ul>
+          {news?.articles?.map(({ title }) => (
+            <li>{title}</li>
           ))}
         </ul>
       </section>
